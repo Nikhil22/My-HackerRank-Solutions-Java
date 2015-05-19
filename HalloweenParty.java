efc2adac5634468218e5700c8f37ca96db16b798
Nikhil Bhaskar <nikhil38@hotmail.com>
2015-05-18 21:28:10 -0400

    Create HalloweenParty.java

---
+++ HalloweenParty.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    
+    static int maxChoc(int k){
+        //base case
+        if(k == 1){
+            return 0;
+        }
+        if(k % 2 == 0){
+            return (k/2) * (k/2);
+        }else { 
+            return ((k-1)/2) *((k-1)/2) + (k-1)/2;
+        }
+    }
+    
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int T = in.nextInt();
+        for(int i = 0; i < T; i++){
+            System.out.println(maxChoc(in.nextInt()));
+        }
+    }
+}
