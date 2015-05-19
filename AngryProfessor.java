2bfb7d85c3204781f4638fe42fc36cd47ddab5be
Nikhil Bhaskar <nikhil38@hotmail.com>
2015-05-18 21:27:44 -0400

    Create AngryProfessor.java

---
+++ AngryProfessor.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    
+    public static String yesNo(int N, int K, int[] arrivals){
+        int count = 0;
+        for(int i =0; i< arrivals.length; i++){
+            if(arrivals[i] <= 0){
+                count++;
+            }
+        }
+        
+        if(count >= K){
+            return "NO";
+        }else{
+            return "YES";
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int T = in.nextInt();
+        int N = 0;
+        int K = 0;
+        for(int i = 0; i < T; i++){
+            N = in.nextInt();
+            K = in.nextInt();
+            int[] arrivals = new int[N];
+            for(int x = 0; x < N; x++){
+               
+               arrivals[x] = in.nextInt(); 
+            }
+            
+            System.out.println(yesNo(N, K, arrivals));
+        }
+    }
+}
