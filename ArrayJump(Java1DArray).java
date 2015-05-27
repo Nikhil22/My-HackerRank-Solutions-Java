03fbd34007a955aa88a281a24b7971590eb75cef
Nikhil Bhaskar <nikhil38@hotmail.com>
2015-05-26 20:58:38 -0400

    Update ArrayJump(Java1DArray).java

--- ArrayJump(Java1DArray).java
+++ ArrayJump(Java1DArray).java
@@ -6,6 +6,17 @@ import java.util.regex.*;
 
 public class Solution {
     
+    /*
+		
+		Step 1: Advance as much as you can (AKA, increment index of array 'a' as long as a[index] =='0', -> keep advancing until the a[index + 1] is a '1')
+		Step 2: Check whether we can just jump and win the game
+		Step 3: If we can't win just yet, check how far we CAN jump (in other words, how far can we jump such that on our landing index, a[index] == 0)
+		Step 4: If we have to take so many steps back that we have essentially NOT MOVED (stepBack == jumpLength) from our position (AKA can't jump) - WE ARE trapped - SO WE LOSE
+		Step 5: Else, jump as far as we can, and increment our index.
+		Step 6: Repeat steps 1-5 
+		
+	*/
+    
     static String jumpAndWin(int[] a, int jumpLength){
         int i = 0;
         int stepBack;
