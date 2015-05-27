ef999deffbed97b2eab91da46838b1bedffa6ef5
Nikhil Bhaskar <nikhil38@hotmail.com>
2015-05-27 11:29:38 -0400

    Update CutTheSticks.java

--- CutTheSticks.java
+++ CutTheSticks.java
@@ -6,6 +6,18 @@ import java.util.regex.*;
 
 public class Solution {
     
+    /*
+	
+	Sort the array so that the smallest elements are at the front
+	Keep track of the minimum (should be array[0] since it has been sorted)	
+	Loop through the array, and subtract the minimum from each element of the array
+	Keep track of the number of 0's that result from this subtraction
+	If the result of thr subtraction is NOT == 0, this indicates we have reached a value higher than our minimum
+	Thus, assign this new value to the minimum.
+	Repeat this process until the entire array has been looped through 
+	
+	*/
+    
     static void stix(int[] a){
         int cuts = a.length;
         System.out.println(cuts);
