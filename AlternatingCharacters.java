fd6575d2ee4cbd710bd3c66f0ab1e6571e687585
Nikhil Bhaskar <nikhil38@hotmail.com>
2015-05-26 21:17:54 -0400

    Update AlternatingCharacters.java

--- AlternatingCharacters.java
+++ AlternatingCharacters.java
@@ -6,6 +6,14 @@ import java.util.regex.*;
 
 public class Solution {
     
+    /*
+		
+	Start From index 1 of the string, since we'll be comparing the character of the string at index i with 
+	the character of the string at index i-1
+	If two consecutive characters are equal, increment the number of minimum deletions needed
+		
+	*/
+    
     static int minDeletions(String s){
         int minDeletes = 0;
         for(int i = 1; i < s.length(); i++){
