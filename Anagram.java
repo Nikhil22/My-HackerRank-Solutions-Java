f11bbb3a2605a77ade28274762cba778c04d1163
Nikhil Bhaskar <nikhil38@hotmail.com>
2015-05-26 20:47:56 -0400

    Update Anagram.java

--- Anagram.java
+++ Anagram.java
@@ -6,6 +6,14 @@ import java.util.regex.*;
 
 public class Solution {
     
+    /*
+		
+		Divides the string into two parts
+		For each character in the left half of the string, check if the right half contains it
+		If the right half does not contain, incrememnt the number of replacements needed by 1
+		
+	*/
+    
     static int anagram(String abc){
         if(abc.length() % 2 == 0){
             int replacements = 0;
