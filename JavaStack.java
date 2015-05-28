7b0fb30f37998dca76e53e930102bfbaa628ec02
Nikhil Bhaskar <nikhil38@hotmail.com>
2015-05-27 22:35:53 -0400

    Update JavaStack.java

--- JavaStack.java
+++ JavaStack.java
@@ -5,20 +5,31 @@ import java.math.*;
 import java.util.regex.*;
 
 public class Solution {
+	
+	/*
+	Loop through the entire string
+	If the character in the string is a '{', '(' or '[', push it onto the stack
+	If the character in the string is none of the above, then peek at the top of the stack
+		If the top of the stack does NOT contain the matching opening bracket, (ie. '{' matches '}'), return false
+		Else, pop it off the stack
+	Once string has been looped through, check if the stack is empty
+	If the stack is empty, return true 
+	Else, return false
+	*/
     
-    static boolean isBalanced(String paranthesis){
+    static boolean isBalanced(String string){
         Stack stack = new Stack();
-        for(int i = 0; i < paranthesis.length(); i++){
-            if(paranthesis.charAt(i) == '{'
-              || paranthesis.charAt(i) == '('
-              || paranthesis.charAt(i) == '['){
-                stack.push(new Character(paranthesis.charAt(i)));
+        for(int i = 0; i < string.length(); i++){
+            if(string.charAt(i) == '{'
+              || string.charAt(i) == '('
+              || string.charAt(i) == '['){
+                stack.push(new Character(string.charAt(i)));
 
-            } else{ 
+            } else{ //IGT why you need an 'else' here
             
             if(!stack.empty()){  
             
-                if(paranthesis.charAt(i) == '}'
+                if(string.charAt(i) == '}'
                    && (char)stack.peek() != '{'){
                         return false;
                 }else{
@@ -28,7 +39,7 @@ public class Solution {
             
             if(!stack.empty()){ 
             
-                if(paranthesis.charAt(i) == ']'
+                if(string.charAt(i) == ']'
                     && (char)stack.peek() != '['){
                         return false;
                 }else{
@@ -38,7 +49,7 @@ public class Solution {
             
             if(!stack.empty()){ 
             
-                if(paranthesis.charAt(i) == ')'
+                if(string.charAt(i) == ')'
                     && (char)stack.peek() != '('){
                         return false;
                 }else{
