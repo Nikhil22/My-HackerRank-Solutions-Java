868a20c969dd8bf8e508937e2b030e1cffdb0e5f
Nikhil Bhaskar <nikhil38@hotmail.com>
2015-05-30 11:23:40 -0400

    Update UtopianTree.java

--- UtopianTree.java
+++ UtopianTree.java
@@ -4,20 +4,27 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class Solution {
-    final static int sh = 1;
-public static void main(String[] args){
-    present(); 
-}
+    public class Solution {
+        
+    /*
+    Establish the base cases (shown below)
+    int cycles - the number of growth cycles
+    int height - starting height (starts off at 1)
+    Loop starting from int i = 1 until i is less than or equal to cycles
+    If i is odd then we are in spring, so height = height * 2
+    If i is even then we are in summer, so height = height + 1
+    Output height once the loop is complete
+    */
+        
     public static int getHeight(int cycles){
         int height = 1;
         //base cases
         if(cycles == 1){
-            return sh*2;
+            return height*2;
         }else if(cycles == 2){
-            return sh*2 +1;
+            return height*2 +1;
         }else if(cycles == 0){
-            return sh;
+            return height;
         }
         
         for(int i = 1; i <=cycles; i++){
@@ -31,16 +38,14 @@ public static void main(String[] args){
         return height; 
         
     }
-    
-    public static void present(){
-        Scanner in = new Scanner(System.in);
-        int t;
-        t= in.nextInt(); //number of test cases
-        int cycles = 0;
-        int b;
         
-        for(int i = 0; i < t; i++){
-            cycles = in.nextInt();
+    
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int T = in.nextInt();
+        
+        for(int i = 0; i < T; i++){
+            int cycles = in.nextInt();
             System.out.println(getHeight(cycles));
         }
     }
