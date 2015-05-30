212cd26ec56ca1e0e3e97d38c28462d0901f9a0d
Nikhil Bhaskar <nikhil38@hotmail.com>
2015-05-29 21:39:18 -0400

    Update MakeItAnagram.java

--- MakeItAnagram.java
+++ MakeItAnagram.java
@@ -6,6 +6,15 @@ import java.util.regex.*;
 
 public class Solution {
     
+	/*
+	Take two strings are inputs: Call it string 'a' and string 'b'
+	Keep track of the number of deletions needed - call it int 'deletions'
+	Loop through string 'b'
+	For each character in string 'b', check if string 'a' contains that character
+	If string 'a' does not contain the character, increment deletions by 1
+	Return deletions * 2 because for every character we delete from one string, we must delete oe from the other string
+	*/
+    
     static int deletions(String a, String b){
         int deletions = 0;
         
