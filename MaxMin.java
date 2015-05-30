7b14e6e315e377ef40ae2aa59ea1f866115bd312
Nikhil Bhaskar <nikhil38@hotmail.com>
2015-05-29 22:19:48 -0400

    Update MaxMin.java

--- MaxMin.java
+++ MaxMin.java
@@ -1,4 +1,3 @@
-
 import java.util.Arrays;
 import java.io.*;
 import java.util.*;
@@ -8,18 +7,28 @@ import java.util.regex.*;
 
 public class Solution {
     
+    /*
+    Call the length of the subarray 'group'
+    Set 'unfairness' to the highest possible integer value (Integer.MAX_VALUE)
+    Sort the array
+    Loop through the array BUT..
+    Start at index 0, and loop till index group -1
+    Check if group -1 + i < the array length (to see if we have a new max/min to compare)
+    If so, check if the current unfairness > (max number in the group - min number in the group)
+    If so, set unfairness to that value (so, unfairness = list[group -1 + i] - list[i])
+    Output 'unfairness' when the array has been fully looped through
+    
+    */
+    
     static void minUnfairness(int group, int[] list){
         Arrays.sort(list);
         
-        int unfairness = Integer.MAX_VALUE;
-        int indexEnd = 0;
-        
+        int unfairness = Integer.MAX_VALUE;       
         
            for(int i = 0; i < group -1 + i; i++){
                if((group - 1 + i) < list.length){
                    if(unfairness > list[group -1 + i] - list[i]){
                     unfairness = list[group -1 + i] - list[i];
-                    indexEnd = group -1 + i;
                     } 
                }
             
@@ -44,3 +53,4 @@ public class Solution {
      
     }   
 }
+
