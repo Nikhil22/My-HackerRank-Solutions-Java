3d3fbb3ad824a1f15ba6844bab1c974fff1bf555
Nikhil Bhaskar <nikhil38@hotmail.com>
2015-06-07 17:15:34 -0400

    Update IsFibo.java

--- IsFibo.java
+++ IsFibo.java
@@ -11,6 +11,8 @@ public class Solution {
 	Step 1: Find 2 numbers (call them int n, nLess) that sum to our input x, 
 		such that it makes sense in a fibonacci sense - What do I mean by that?...
 		The difference between n and nLess cannot greater than nLess (to satisfy fibonacci sequence)
+		To do this, loop while (n - nLess > nLess)
+			For each loop, decrement n and increment nLess
 		So we now have a seqeunce nLess, n, x  which satisfy fibo incremental conditions (may not be fibo, though)
 	Step 2: Now it's easy to find the number that comes before nLess (call it nEvenLess)
 		While nEvenLess > 0, decrement the values of nLess and nEvenLess such that they satisfy the fibo sequence
