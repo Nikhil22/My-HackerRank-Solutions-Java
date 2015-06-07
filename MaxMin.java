cc8e9a829fbbb3f4c853732b1ccc4b8c6806a496
Nikhil Bhaskar <nikhil38@hotmail.com>
2015-06-07 17:35:38 -0400

    Update MaxMin.java

--- MaxMin.java
+++ MaxMin.java
@@ -12,7 +12,7 @@ public class Solution {
     Set 'unfairness' to the highest possible integer value (Integer.MAX_VALUE)
     Sort the array
     Loop through the array BUT..
-    Start at index 0, and loop till index group -1
+    Start at index 0, and loop till index group -1 + i
     Check if group -1 + i < the array length (to see if we have a new max/min to compare)
     If so, check if the current unfairness > (max number in the group - min number in the group)
     If so, set unfairness to that value (so, unfairness = list[group -1 + i] - list[i])
