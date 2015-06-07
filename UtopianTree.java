18725b2158993caf1803a44353c5ec7ed078388b
Nikhil Bhaskar <nikhil38@hotmail.com>
2015-06-07 18:36:13 -0400

    Update UtopianTree.java

--- UtopianTree.java
+++ UtopianTree.java
@@ -7,10 +7,10 @@ import java.util.regex.*;
     public class Solution {
         
     /*
-    Establish the base cases (shown below)
+    Establish the base case (shown below)
     int cycles - the number of growth cycles
     int height - starting height (starts off at 1)
-    Loop starting from int i = 1 until i is less than or equal to cycles
+    Loop starting from int i = 1 until i is less than or equal to 'cycles'
     If i is odd then we are in spring, so height = height * 2
     If i is even then we are in summer, so height = height + 1
     Output height once the loop is complete
@@ -18,17 +18,14 @@ import java.util.regex.*;
         
     public static int getHeight(int cycles){
         int height = 1;
-        //base cases
-        if(cycles == 1){
-            return height*2;
-        }else if(cycles == 2){
-            return height*2 +1;
-        }else if(cycles == 0){
+        
+        //base case
+        if(cycles == 0){
             return height;
         }
         
         for(int i = 1; i <=cycles; i++){
-            if(i % 2 != 0){ //if odd, then spring
+            if(i % 2 != 0){ 
                 height = height *2;
             }else if(i % 2 == 0){
                 height = height +1;
